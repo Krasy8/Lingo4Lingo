8c68f5c3cd5261862f64bdd84247972e00e74aa5
Konrad_Krasowski <krasy8@gmail.com>
2020-07-02 13:10:52 +0100

    1.Registration form structure corrected into 2 columns inside 1 row, which makes switching between fields more logical-from top to bottom instead of from left to right; 2.Header css improved.

--- Lingo4Lingo/FrontEnd/src/components/Header/Header.js
+++ Lingo4Lingo/FrontEnd/src/components/Header/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 import './Header.css';
 
 const header = () => (
-  <h1 className="site-heading text-center text-white d-none d-lg-block">
-    <span className="site-heading-upper text-primary mb-3">
-      Learn by meeting new people
-    </span>
-    <span className="site-heading-lower">Lingo4Lingo</span>
-  </h1>
+  <div className="product-item-title d-flex div-main">
+    <div className="bg-faded p-5 d-flex ml-auto rounded div-heading">
+      <div>
+        <h1 className="site-heading text-center text-white d-none d-lg-block heading-center">
+          <span className="site-heading-upper text-primary mb-3">
+            Learn by meeting new people
+          </span>
+          <span className="site-heading-lower">Lingo4Lingo</span>
+        </h1>
+      </div>
+    </div>
+  </div>
 );
 
 export default header;
